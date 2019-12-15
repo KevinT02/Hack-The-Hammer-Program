c6259af79aed0561479929e9d7f786e455aedf5e
Kevin Tu <tu02.kevin@gmail.com>
2019-12-14 23:14:57 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,1 @@
-from Crypto.Cipher import Blowfish
-cipher = Blowfish.new("key must be 4 to 56 bytes")
-encrypted_data = cipher.encrypt("1234hack")
 
-decrypted_data = cipher.decrypt(encrypted_data)
-decrypted_data = decrypted_data.decode("utf-8")
-
-print ("Encrypted Text: ", encrypted_data)
-print ("Decrypted Text: ", decrypted_data)
