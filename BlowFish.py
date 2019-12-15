fb985347053109b486e1caf269393f284a37d25e
Sahil0702 <58314419+Sahil0702@users.noreply.github.com>
2019-12-15 09:22:18 -0500

    Update BlowFish.py

--- BlowFish.py
+++ BlowFish.py
@@ -7,3 +7,27 @@ decrypted_data = decrypted_data.decode("utf-8")
 
 print ("Encrypted Text: ", encrypted_data)
 print ("Decrypted Text: ", decrypted_data)
+
+
+
+
+
+
+
+from Crypto.Cipher import Blowfish
+
+def PKCS5Padding(string):
+    byteNum = len(string)
+    packingLength = 8 - byteNum % 8
+    if packingLength == 8:
+        return string
+    else:
+        appendage = chr(packingLength) * packingLength
+        return string + appendage
+
+def PandoraEncrypt(string):
+    from Crypto.Cipher import Blowfish
+    key = b'6#26FRL$ZWD'
+    c1  = Blowfish.new(key, Blowfish.MODE_ECB)
+    packedString = PKCS5Padding(string)
+    return c1.encrypt(packedString)
